30448ca2c6346b01a13f822203a92f40951872ad
skyrocoster <91031422+skyrocoster@users.noreply.github.com>
2023-01-20 21:38:39 +0000

    20/01/2023

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,30 +12,30 @@ header = st.container()
 dataset = st.container()
 features = st.container()
 
-player_matches = object_as_df(PlayerMatches().query.all())
-player_matches["hero_name"] = player_matches["hero_id"].map(Heroes().hero_map())
-player_matches_first_session = (
-    player_matches.sort_values(["start_time"], ascending=True)
+grubby_matches = object_as_df(PlayerMatches().query.all())
+grubby_matches["hero_name"] = grubby_matches["hero_id"].map(Heroes().hero_map())
+grubby_matches_first_session = (
+    grubby_matches.sort_values(["start_time"], ascending=True)
     .groupby("session_id")
     .first()
     .reset_index()
 )
-player_matches_first_session["day_of_week"] = player_matches_first_session[
+grubby_matches_first_session["day_of_week"] = grubby_matches_first_session[
     "start_time_normal"
 ].dt.day_name()
-player_matches["session_day_of_week"] = player_matches["session_id"].map(
+grubby_matches["session_day_of_week"] = grubby_matches["session_id"].map(
     dict(
         zip(
-            player_matches_first_session["session_id"],
-            player_matches_first_session["day_of_week"],
+            grubby_matches_first_session["session_id"],
+            grubby_matches_first_session["day_of_week"],
         )
     )
 )
-player_matches["session_start"] = player_matches["session_id"].map(
+grubby_matches["session_start"] = grubby_matches["session_id"].map(
     dict(
         zip(
-            player_matches_first_session["session_id"],
-            player_matches_first_session["start_time_normal"],
+            grubby_matches_first_session["session_id"],
+            grubby_matches_first_session["start_time_normal"],
         )
     )
 )
@@ -47,27 +47,20 @@ all = st.sidebar.checkbox("Select all")
 if all:
     hero = st.sidebar.multiselect(
         "Select Heroes:",
-        player_matches["hero_name"].sort_values().unique(),
-        player_matches["hero_name"].sort_values().unique(),
+        grubby_matches["hero_name"].sort_values().unique(),
+        grubby_matches["hero_name"].sort_values().unique(),
     )
 else:
     hero = st.sidebar.multiselect(
         "Select Heroes:",
-        player_matches["hero_name"].sort_values().unique(),
+        grubby_matches["hero_name"].sort_values().unique(),
     )
 
-# st.sidebar.slider(
-#     "Test",
-#     value=[
-#         player_matches["start_time_normal"].min(),
-#         player_matches["start_time_normal"].max(),
-#     ],
-# )
 
 game_mode = st.sidebar.multiselect("Select Lobby Type", options=[0, 7], default=7)
 
 played_ranks = (
-    player_matches["average_rank"].dropna().astype(int).sort_values().unique()
+    grubby_matches["average_rank"].dropna().astype(int).sort_values().unique()
 )
 played_rank_ends = [int(played_ranks.min()), int(played_ranks.max())]
 
@@ -80,50 +73,74 @@ ranks = st.sidebar.slider(
 
 # MAIN
 
-player_matches_sel = player_matches.loc[
-    (player_matches["hero_name"].isin(hero))
-    & (player_matches["average_rank"] >= ranks[0])
-    & (player_matches["average_rank"] <= ranks[1])
-    & (player_matches["lobby_type"].isin(game_mode))
+grubby_matches_sel = grubby_matches.loc[
+    (grubby_matches["hero_name"].isin(hero))
+    & (grubby_matches["average_rank"] >= ranks[0])
+    & (grubby_matches["average_rank"] <= ranks[1])
+    & (grubby_matches["lobby_type"].isin(game_mode))
 ]
 
 
 see_data = st.expander("Raw Data Here!")
 with see_data:
-    st.dataframe(data=player_matches_sel)
+    st.dataframe(data=grubby_matches_sel)
 
 day_info = (
-    player_matches_sel[["session_day_of_week", "win"]]
+    grubby_matches_sel[["session_day_of_week", "win"]]
     .groupby("session_day_of_week")
     .mean()
-    .sort_values("win", ascending=False)
+    .sort_values("session_day_of_week", ascending=False)
     .reset_index()
 )
 fig = px.bar(day_info, x="session_day_of_week", y="win", labels=True, text_auto=True)
-
-st.plotly_chart(fig)
-
-hero_wr = player_matches_sel[["hero_name", "win"]].copy()
-hero_wr = hero_wr.groupby("hero_name").mean().reset_index()
-
-player_matches_sel_hero_played = (
-    player_matches_sel[["hero_name"]].value_counts().reset_index()
+pie = px.pie(
+    grubby_matches_sel[["duration", "session_day_of_week"]],
+    values="duration",
+    names="session_day_of_week",
 )
-player_matches_sel_hero_played.columns = ["hero_name", "times_played"]
-hero_wr = hero_wr.merge(player_matches_sel_hero_played, on="hero_name")
+
+days_col1, days_col2 = st.columns(2)
+with days_col1:
+    st.title("Win Rate on Stream Days")
+    st.plotly_chart(fig)
+with days_col2:
+    st.title("Time Played on Stream Days")
+    st.plotly_chart(pie)
+
+hero_wr = (
+    grubby_matches_sel[["hero_name", "win"]].groupby("hero_name").mean().reset_index()
+)
+# hero_wr = hero_wr
+
+grubby_matches_sel_hero_played = (
+    grubby_matches_sel[["hero_name"]].value_counts().reset_index()
+)
+grubby_matches_sel_hero_played.columns = ["hero_name", "times_played"]
+hero_wr = hero_wr.merge(grubby_matches_sel_hero_played, on="hero_name")
 
 win_loss = (
-    str(player_matches_sel.loc[player_matches_sel["win"] == 1].count()["win"])
+    str(grubby_matches_sel.loc[grubby_matches_sel["win"] == 1].count()["win"])
     + " Wins - "
-    + str(player_matches_sel.loc[player_matches_sel["win"] == 0].count()["win"])
+    + str(grubby_matches_sel.loc[grubby_matches_sel["win"] == 0].count()["win"])
     + " Losses"
 )
 
+overall_wr = grubby_matches["win"].mean()
+sel_wr = grubby_matches_sel["win"].mean()
+
+st.markdown("""---""")
 hero_wr_col1, hero_wr_col2 = st.columns(2)
 fig = px.bar(hero_wr, x="hero_name", y="win", color="times_played")
+fig.add_hline(
+    y=sel_wr,
+    line_color="green",
+    annotation_text="Selection Winrate",
+    annotation_position="bottom right",
+)
 with hero_wr_col1:
     st.title("Hero Win Rates")
-    st.text(win_loss)
     st.plotly_chart(fig)
 with hero_wr_col2:
+    st.title("Win Rates Table")
+    st.text(win_loss)
     st.dataframe(hero_wr)
